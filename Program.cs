b80a9b51c6fbfe994736fb503dbcfebb4d3b7ca5
LinathiMrulekana <102727009+LinathiMrulekana@users.noreply.github.com>
2022-05-19 09:34:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversStr = string.Empty;
+            string inputStr = string.Empty;
+            Console.Write("Enter a name: ");
+            inputStr = Console.ReadLine();
+            if (inputStr != null)
+            {
+                for (int i = inputStr.Length - 1; i >= 0; i--)
+                {
+                    reversStr += inputStr[i].ToString();
+                }
+                if (reversStr == inputStr)
+                {
+                    Console.WriteLine("String is palindrome input = {0} and Output = {1}", inputStr , reversStr);
+                }
+                else
+                {
+                    Console.WriteLine("String is not a palindrome Input = {0} and Output = {1}", inputStr , reversStr);
+                }
+            }
+         Console.ReadLine();
+        }
+    }
+}
+ 
